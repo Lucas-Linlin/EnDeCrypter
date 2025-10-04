53bd3a26fa9002351e9c987eef1e5ab93e3627bb
Lucas-Linlin <zhilin.tang@qq.com>
2025-10-04 08:02:05 +0800

    fix(src): 修复密码输入框位置及添加解密区功能
    密码输入框位置调整，移至更合理位置，并重新命名相关组件。添加了解密区的功能，包含密码输入框、方法输入框、消息标签、方法文件组、输出文本框和解密按钮。同时添加了密码可见性切换按钮，并更新了相应的图标和文本。
    
    - 将 `QLineEdit` 和 `QToolButton` 添加到加密区布局中，调整 `psw_input1` 的位置。
    - 添加 `QToolButton` (`pswToggle_button2_2`) 用于切换密码可见性，并更新图标路径。
    - 重命名 `tab_2` 为 `decrypt_zone`，并为其添加解密区布局。
    - 在解密区布局中添加 `msg_label2`, `msg_input2`, `mtd_input2`, `pswToggle_button2`, `mtdFile2`, `out_text2` 和 `decrypt_button`。
    - 更新 `main_tab` 的 `toolTip` 属性，使其分别指向加密区和解密区。

--- src/edcterGUI.py
+++ src/edcterGUI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'edcterGUIkRccZO.ui'
+## Form generated from reading UI file 'edcterGUIxNFgis.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLabel,
     QLineEdit, QPushButton, QSizePolicy, QTabWidget,
-    QTextEdit, QWidget)
+    QTextEdit, QToolButton, QWidget)
 
 class Ui_root(object):
     def setupUi(self, root):
@@ -73,12 +73,6 @@ class Ui_root(object):
         self.out_label1.setFont(font)
 
         self.ect_layout.addWidget(self.out_label1, 4, 0, 1, 1)
-
-        self.psw_input1 = QLineEdit(self.gridLayoutWidget_2)
-        self.psw_input1.setObjectName(u"psw_input1")
-        self.psw_input1.setFont(font)
-
-        self.ect_layout.addWidget(self.psw_input1, 3, 1, 1, 2)
 
         self.msg_label1 = QLabel(self.gridLayoutWidget_2)
         self.msg_label1.setObjectName(u"msg_label1")
@@ -140,6 +134,21 @@ class Ui_root(object):
 
         self.ect_layout.addWidget(self.encrypt_button, 5, 0, 1, 3)
 
+        self.psw_input1 = QLineEdit(self.gridLayoutWidget_2)
+        self.psw_input1.setObjectName(u"psw_input1")
+        self.psw_input1.setFont(font)
+
+        self.ect_layout.addWidget(self.psw_input1, 3, 1, 1, 1)
+
+        self.pswToggle_button2_2 = QToolButton(self.gridLayoutWidget_2)
+        self.pswToggle_button2_2.setObjectName(u"pswToggle_button2_2")
+        icon = QIcon()
+        icon.addFile(u"../../images/disabled.ico", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        icon.addFile(u"../../images/enabled.ico", QSize(), QIcon.Mode.Normal, QIcon.State.On)
+        self.pswToggle_button2_2.setIcon(icon)
+
+        self.ect_layout.addWidget(self.pswToggle_button2_2, 3, 2, 1, 1)
+
         self.mtd_label1.raise_()
         self.mtd_input1.raise_()
         self.msg_label1.raise_()
@@ -150,31 +159,16 @@ class Ui_root(object):
         self.psw_input1.raise_()
         self.psw_label1.raise_()
         self.mtdFile1.raise_()
+        self.pswToggle_button2_2.raise_()
         self.main_tab.addTab(self.encrypt_zone, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.gridLayoutWidget_3 = QWidget(self.tab_2)
+        self.decrypt_zone = QWidget()
+        self.decrypt_zone.setObjectName(u"decrypt_zone")
+        self.gridLayoutWidget_3 = QWidget(self.decrypt_zone)
         self.gridLayoutWidget_3.setObjectName(u"gridLayoutWidget_3")
         self.gridLayoutWidget_3.setGeometry(QRect(0, 0, 401, 271))
         self.dct_layout = QGridLayout(self.gridLayoutWidget_3)
         self.dct_layout.setObjectName(u"dct_layout")
         self.dct_layout.setContentsMargins(0, 0, 0, 0)
-        self.out_text2 = QTextEdit(self.gridLayoutWidget_3)
-        self.out_text2.setObjectName(u"out_text2")
-
-        self.dct_layout.addWidget(self.out_text2, 4, 1, 1, 1)
-
-        self.msg_label2 = QLabel(self.gridLayoutWidget_3)
-        self.msg_label2.setObjectName(u"msg_label2")
-
-        self.dct_layout.addWidget(self.msg_label2, 0, 0, 1, 1)
-
-        self.msg_input2 = QLineEdit(self.gridLayoutWidget_3)
-        self.msg_input2.setObjectName(u"msg_input2")
-        self.msg_input2.setFont(font)
-
-        self.dct_layout.addWidget(self.msg_input2, 0, 1, 1, 1)
-
         self.out_label2 = QLabel(self.gridLayoutWidget_3)
         self.out_label2.setObjectName(u"out_label2")
 
@@ -185,16 +179,6 @@ class Ui_root(object):
 
         self.dct_layout.addWidget(self.mtd_label2, 1, 0, 1, 1)
 
-        self.mtd_input2 = QLineEdit(self.gridLayoutWidget_3)
-        self.mtd_input2.setObjectName(u"mtd_input2")
-
-        self.dct_layout.addWidget(self.mtd_input2, 1, 1, 1, 1)
-
-        self.decrypt_button = QPushButton(self.gridLayoutWidget_3)
-        self.decrypt_button.setObjectName(u"decrypt_button")
-
-        self.dct_layout.addWidget(self.decrypt_button, 5, 0, 1, 2)
-
         self.psw_input2 = QLineEdit(self.gridLayoutWidget_3)
         self.psw_input2.setObjectName(u"psw_input2")
 
@@ -204,6 +188,28 @@ class Ui_root(object):
         self.psw_label2.setObjectName(u"psw_label2")
 
         self.dct_layout.addWidget(self.psw_label2, 3, 0, 1, 1)
+
+        self.msg_label2 = QLabel(self.gridLayoutWidget_3)
+        self.msg_label2.setObjectName(u"msg_label2")
+
+        self.dct_layout.addWidget(self.msg_label2, 0, 0, 1, 1)
+
+        self.pswToggle_button2 = QToolButton(self.gridLayoutWidget_3)
+        self.pswToggle_button2.setObjectName(u"pswToggle_button2")
+        self.pswToggle_button2.setIcon(icon)
+
+        self.dct_layout.addWidget(self.pswToggle_button2, 3, 2, 1, 1)
+
+        self.msg_input2 = QLineEdit(self.gridLayoutWidget_3)
+        self.msg_input2.setObjectName(u"msg_input2")
+        self.msg_input2.setFont(font)
+
+        self.dct_layout.addWidget(self.msg_input2, 0, 1, 1, 2)
+
+        self.mtd_input2 = QLineEdit(self.gridLayoutWidget_3)
+        self.mtd_input2.setObjectName(u"mtd_input2")
+
+        self.dct_layout.addWidget(self.mtd_input2, 1, 1, 1, 2)
 
         self.mtdFile2 = QGroupBox(self.gridLayoutWidget_3)
         self.mtdFile2.setObjectName(u"mtdFile2")
@@ -227,9 +233,19 @@ class Ui_root(object):
         self.mtd_layout2.addWidget(self.mtdSave_button2, 0, 1, 1, 1)
 
 
-        self.dct_layout.addWidget(self.mtdFile2, 2, 0, 1, 2)
+        self.dct_layout.addWidget(self.mtdFile2, 2, 0, 1, 3)
 
-        self.main_tab.addTab(self.tab_2, "")
+        self.out_text2 = QTextEdit(self.gridLayoutWidget_3)
+        self.out_text2.setObjectName(u"out_text2")
+
+        self.dct_layout.addWidget(self.out_text2, 4, 1, 1, 2)
+
+        self.decrypt_button = QPushButton(self.gridLayoutWidget_3)
+        self.decrypt_button.setObjectName(u"decrypt_button")
+
+        self.dct_layout.addWidget(self.decrypt_button, 5, 0, 1, 3)
+
+        self.main_tab.addTab(self.decrypt_zone, "")
 
         self.main_layout.addWidget(self.main_tab, 0, 0, 1, 2)
 
@@ -247,7 +263,10 @@ class Ui_root(object):
         self.exit_button.setText(QCoreApplication.translate("root", u"\u9000\u51fa", None))
         self.clear_button.setText(QCoreApplication.translate("root", u"\u6e05\u7a7a", None))
 #if QT_CONFIG(tooltip)
-        self.main_tab.setToolTip(QCoreApplication.translate("root", u"<html><head/><body><p>\u52a0\u5bc6\u533a</p></body></html>", None))
+        self.main_tab.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(tooltip)
+        self.encrypt_zone.setToolTip(QCoreApplication.translate("root", u"<html><head/><body><p>\u52a0\u5bc6\u533a</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
         self.mtd_label1.setText(QCoreApplication.translate("root", u"\u65b9\u6cd5", None))
         self.out_label1.setText(QCoreApplication.translate("root", u"\u8f93\u51fa", None))
@@ -257,15 +276,20 @@ class Ui_root(object):
         self.mtdLoad_button1.setText(QCoreApplication.translate("root", u"\u52a0\u8f7d\u73b0\u6709\u65b9\u6cd5\u6587\u4ef6", None))
         self.mtdSave_button1.setText(QCoreApplication.translate("root", u"\u4fdd\u5b58\u5f53\u524d\u65b9\u6cd5", None))
         self.encrypt_button.setText(QCoreApplication.translate("root", u"\u52a0\u5bc6", None))
+        self.pswToggle_button2_2.setText(QCoreApplication.translate("root", u"\u5207\u6362\u5bc6\u7801\u53ef\u89c1\u6027", None))
         self.main_tab.setTabText(self.main_tab.indexOf(self.encrypt_zone), QCoreApplication.translate("root", u"\u52a0\u5bc6\u533a", None))
-        self.msg_label2.setText(QCoreApplication.translate("root", u"\u6d88\u606f", None))
+#if QT_CONFIG(tooltip)
+        self.decrypt_zone.setToolTip(QCoreApplication.translate("root", u"<html><head/><body><p>\u89e3\u5bc6\u533a</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
         self.out_label2.setText(QCoreApplication.translate("root", u"\u8f93\u51fa", None))
         self.mtd_label2.setText(QCoreApplication.translate("root", u"\u65b9\u6cd5", None))
-        self.decrypt_button.setText(QCoreApplication.translate("root", u"\u89e3\u5bc6", None))
         self.psw_label2.setText(QCoreApplication.translate("root", u"\u5bc6\u7801", None))
+        self.msg_label2.setText(QCoreApplication.translate("root", u"\u6d88\u606f", None))
+        self.pswToggle_button2.setText(QCoreApplication.translate("root", u"\u5207\u6362\u5bc6\u7801\u53ef\u89c1\u6027", None))
         self.mtdFile2.setTitle("")
         self.mtdLoad_button2.setText(QCoreApplication.translate("root", u"\u52a0\u8f7d\u73b0\u6709\u65b9\u6cd5\u6587\u4ef6", None))
         self.mtdSave_button2.setText(QCoreApplication.translate("root", u"\u4fdd\u5b58\u5f53\u524d\u65b9\u6cd5", None))
-        self.main_tab.setTabText(self.main_tab.indexOf(self.tab_2), QCoreApplication.translate("root", u"\u89e3\u5bc6\u533a", None))
+        self.decrypt_button.setText(QCoreApplication.translate("root", u"\u89e3\u5bc6", None))
+        self.main_tab.setTabText(self.main_tab.indexOf(self.decrypt_zone), QCoreApplication.translate("root", u"\u89e3\u5bc6\u533a", None))
     # retranslateUi
 
